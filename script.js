bb125e45b6f0aa36e3e16cf98f74bc42aa4fe111
Hicham Amroussi <hicham.amroussi@gmail.com>
2022-09-16 00:19:38 +0100

    added a logo on the navbar + Phone viewport: added a hamburger menu button

---
+++ script.js
@@ -0,0 +1,38 @@
+// Selectors
+const navbarListEl = document.querySelector('.navbar-list');
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+
+// Event Listeners
+window.addEventListener('resize', () => {
+    showNavButton();
+})
+
+hamburgerBtn.addEventListener('click', () => {
+    const icone = hamburgerBtn.querySelector('i');
+
+    if(icone.classList.contains('fa-bars')) {
+        // Change Icon
+        icone.classList.remove('fa-bars');
+        icone.classList.add('fa-xmark');
+
+        // Slide out the menu
+        navbarListEl.style.transform = "translateX(0)";
+
+    } else if(icone.classList.contains('fa-xmark')) {
+        // Change Icon
+        icone.classList.remove('fa-xmark');
+        icone.classList.add('fa-bars');
+
+        // Slide in the menu
+        navbarListEl.style.transform = "translateX(100vw)";
+    }
+})
+
+// Functions
+function showNavButton() {
+    if(window.innerWidth <= 700) {
+        navbarListEl.classList.add('hamburger-menu');
+    } else {
+        navbarListEl.classList.remove('hamburger-menu');
+    }
+}
