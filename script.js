0c81c9e9ea799c6a7dbb978f636a1adb98cf1631
Hicham Amroussi <hicham.amroussi@gmail.com>
2022-12-11 20:28:38 +0100

    New Welcome Page + Project Section stack design change + Added a Contact Section + New Footer where I moved Links to my social networks

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 // Selectors
 const navbarListEl = document.querySelector('.navbar-list');
 const hamburgerBtn = document.querySelector('.hamburger-btn');
+const flowerPetals = document.querySelectorAll('.forms');
+// Popup
+const popupContainer = document.querySelector('.popup-container');
+const popupText = document.querySelector('.popup-text');
+const popupBtn = document.querySelector('.popup-btn');
+// DOM Form elements
+const myForm = document.getElementById('myForm');
+const confirmBtn = document.querySelector('.submit-btn');
+
+// Functions
 
 // Event Listeners
+flowerPetals.forEach(((petal) => {
+    petal.addEventListener('click', () => {
+        petal.style.translate = "-20px";
+        petal.style.opacity = "0";
+    })
+}))
+
 hamburgerBtn.addEventListener('click', () => {
     const icone = hamburgerBtn.querySelector('i');
 
@@ -24,4 +41,45 @@ hamburgerBtn.addEventListener('click', () => {
     }
 })
 
-// Functions
+myForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const confirmBtn = document.querySelector('.submit-btn');
+
+    // Display Loader inside Form
+    const loader = document.createElement('div');
+    loader.classList.add('circle');
+    confirmBtn.remove();
+    myForm.appendChild(loader);
+
+    // Form Data
+    const formData = new FormData(myForm);
+    const searchParams = new URLSearchParams();
+
+    for(const pair of formData) {
+        searchParams.append(pair[0], pair[1]);
+    }
+
+    fetch('http://localhost:3000/messages', {
+        method: 'POST',
+        body: searchParams
+    }).then((resp) => resp.json())
+    .then((data) => {
+        const loadingAnimation = document.querySelector('.circle');
+        // Show Popup
+        popupText.innerText = data.message;
+        popupContainer.style.display = "flex";
+
+        popupBtn.addEventListener('click', () => {
+            // Remove Popup
+            popupContainer.style.display = "none";
+        })
+
+        // Remove Loader and Bring back button
+        const confirmBtn = document.createElement('button');
+        confirmBtn.textContent = "Submit";
+        confirmBtn.classList.add('submit-btn');
+        loadingAnimation.remove();
+        myForm.appendChild(confirmBtn);
+    });
+})
