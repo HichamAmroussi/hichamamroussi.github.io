0b204fce38b865b14e836d1d9075fc9b2d7a7e43
Hicham Amroussi <hicham.amroussi@gmail.com>
2023-05-09 14:47:40 +0100

    Added a Testimonials Section

--- script.js
+++ script.js
@@ -2,32 +2,51 @@
 //Navbar
 const navbarListEl = document.querySelector('.navbar-list');
 const hamburgerBtn = document.querySelector('.hamburger-btn');
-//Form
-const myForm = document.getElementById('myForm');
-//Page
+// Section 1
 const flowerPetals = document.querySelectorAll('.forms');
+// Section 4
+const testimonialsContainer = document.querySelector('.testimonials-container');
+// Section 5
+const myForm = document.getElementById('myForm');
 //popup
 const popupContainer = document.querySelector('.popup-container');
 const popupText = document.querySelector('.popup-text');
 const popupBtn = document.querySelector('.popup-btn');
 
+//------------- Function Calls
+showTestimonials();
+
 //------------- Event Listeners
 
-document.addEventListener('DOMContentLoaded', incrementPageView);
+//document.addEventListener('DOMContentLoaded', incrementPageView);
+// Forms
 myForm.addEventListener('submit', sendForm);
-hamburgerBtn.addEventListener('click', navigationDrawer);
+// DOM Interactions
 popupBtn.addEventListener('click', () => { popupContainer.style.display = "none" });
+hamburgerBtn.addEventListener('click', showNavigationBar);
 flowerPetals.forEach((petal) => petal.addEventListener('click', () => pickPetal(petal)));
 
 
 //------------- Functions
+//API
+async function incrementPageView() {
+    await fetch("https://ha-portfolio-api.cyclic.app/portfolio/data/pageview", { method: 'PUT' });
+}
 
+async function getTestimonials() {
+    const api = await fetch("https://ha-portfolio-api.cyclic.app/testimonial");
+    const data = await api.json();
+
+    return data.testimonials;
+}
+
+//Other
 function pickPetal(petal) {
     petal.style.translate = "-20px";
     petal.style.opacity = "0";
 }
 
-function navigationDrawer() {
+function showNavigationBar() {
     const icone = hamburgerBtn.querySelector('i');
 
     if (icone.classList.contains('fa-bars')) {
@@ -91,6 +110,25 @@ function showPopup(message) {
     popupContainer.style.display = "flex";
 }
 
-async function incrementPageView() {
-    await fetch("https://ha-portfolio-api.cyclic.app/portfolio/data/pageview", { method: 'PUT' });
+async function showTestimonials() {
+    const testimonialsData = await getTestimonials();
+
+    // Remove Loading Animation
+    testimonialsContainer.querySelector('.circle').remove();
+
+    testimonialsData.forEach((testimonial) => {
+        const testimonialCard = document.createElement('div');
+        testimonialCard.classList.add('testimonial-card');
+
+        testimonialCard.innerHTML = `
+            <div class="random-profile"><i class="fa-solid fa-user"></i></div>
+            <p>“${testimonial.body}”</p>
+            <div>
+                <h4>${testimonial.name}</h4>
+                <p>${testimonial.business}</p>
+            </div>
+        `
+
+        testimonialsContainer.appendChild(testimonialCard);
+    })
 }
